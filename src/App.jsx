e0f639a6a09e7f976441c56d2dff656529c5c952
QrV1t9 <qrvite@gmail.com>
2024-07-31 12:54:00 +0300

    Added an animation with the appearance of the nickname

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import github from "./icons/github.png"
 import telegram from "./icons/telegram.png"
+import {useEffect, useState} from "react";
 
 export default function App() {
+    const [display, setDisplay] = useState("opacity-0")
+    useEffect(() => {
+        setTimeout(() => {
+            setDisplay("opacity-100")
+        }, 500)
+    }, [])
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen container min-w-full">
         <section>
             <div className="text-text/75 font-medium"><span className="text-53px">Hello</span>, <span className="text-32px">i am</span></div>
-            <div className="text-text text-8xl font-extrabold relative right-3 pt-1">QrV1t9</div>
+            <div className={"transition-opacity duration-700 text-text/100 text-8xl font-extrabold relative right-3 pt-1 " + display}>QrV1t9</div>
             <div className="text-text/75 text-36px font-medium">Front-end developer</div>
             <div className="text-text/75 text-36px pt-7 text-center font-medium">My socials:</div>
         </section>
